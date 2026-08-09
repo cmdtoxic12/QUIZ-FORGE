c54b3da78b86067bea5e93546b2d3770717cb214
cmdtoxic12 <promiseability845@gmail.com>
2026-08-09 17:14:50 +0000

    v2.2.0

---
+++ public/js/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCp0KbklA4vyL-4LMSY3y_SFU_W8yCspgU",
+  authDomain: "c-licon-data-bank.firebaseapp.com",
+  projectId: "c-licon-data-bank",
+  storageBucket: "c-licon-data-bank.firebasestorage.app",
+  messagingSenderId: "499812180714",
+  appId: "1:499812180714:web:e088fe476be4b82e732d93",
+  measurementId: "G-EWF9HGN3GY",
+};
